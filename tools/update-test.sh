80b98b1df56a7e108577d942a1f1bea3a63679d8
Rob Palkowski <dopeghoti@gmail.com>
2012-05-04 14:05:33 -0700

    Added my local update-and-compile-the-game script
    You will have to edit it as it's completely
    ground-upped for my system.
    
    Supports copies of both the beta and stable linux BYOND servers

---
+++ tools/update-test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+DMEFILE=baystation12.dme
+
+if [[ 0 -eq $# ]]; then
+   echo "Please run the $0 with the correct paramaters:"
+   echo -e "\t$0 [ beta | stable ]"
+   exit 0
+else
+   case $1 in
+      "beta")
+         MODE=beta
+         ;;
+      "stable")
+         MODE=stable
+         ;;
+      *)
+         echo "Mode $0 not known.  Please use the following syntax:"
+         echo -e "\t$0 [ beta | stable ]"
+         exit 0
+         ;;
+   esac
+   BYOND=/home/ghoti/byond/$MODE/byond/
+fi
+
+echo "Copying source from git repository.."
+rsync -n -zah --stats --delete /home/ghoti/src/daedalus/ /home/ghoti/byond/daedalus-src/
+echo $MODE > /home/ghoti/byond/daedalus-test/mode
+
+echo "Setting up build environment.."
+source $BYOND/bin/byondsetup
+echo "Building BYOND world.."
+DreamMaker /home/ghoti/byond/daedalus-src/$DMEFILE
