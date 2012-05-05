02e8016c496c9cd7f5633484609c2a9483a537e9
Rob Palkowski <dopeghoti@gmail.com>
2012-05-05 10:35:39 -0700

    sourcedive now also searches files _names_ as well as content.

--- tools/sourcedive.sh
+++ tools/sourcedive.sh
@@ -4,10 +4,15 @@
 #  files containing the speficied word.  It will perform a case-insentitive
 #  search from $CWD.  It is adviseable to search from the root of the repo
 #  directory tree (i. e. not within /code/).
+#  It will also search for any file names or directory names matching the
+#  the criteria given.
 
 if [[ 0 -eq $# ]]; then
    echo -e "Usage:\t$0 $HAYSTACK"
    exit 1
 fi
 
+echo "Files containing '$*':"
 grep -Ri "$*" * | cut -d ':' -f 1 | sort | uniq
+echo "Filenames matching '$*':"
+find ./ -name *"$*"*
